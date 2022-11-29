07fe2c8a2c157b0af474dbb4874de97ef4469edb
Davidson Bpe <davidsomixer@gmail.com>
2022-11-29 13:34:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: "#app",
+  data: {
+    nuber: "25",
+    thum: "https://image.thum.io/get/image/fit/300x300/",
+    posts: []
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios.get("https://dl.dropboxusercontent.com/s/rv8j8yd1cho43sm/instagram-receitas.json").then((posts) => {
+        this.posts = posts.data;
+        console.log(this.posts);
+      });
+    }
+  }
+});
